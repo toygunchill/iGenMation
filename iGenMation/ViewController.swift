90a3d02fa737ddda705adcaa3fe3cc96ac94dafb
Toygun Çil <toyguncil1@gmail.com>
2023-12-13 23:48:57 +0300

    just started creating files 📁

---
+++ iGenMation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iGenMation
+//
+//  Created by Toygun Çil on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
